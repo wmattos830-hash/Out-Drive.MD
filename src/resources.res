e296de05e4dd6d2c94d5eef1591e19b8cc31433c
wmattos830-hash <wmattos830@gmail.com>
2026-08-21 22:50:27 -0300

    Add files via upload

---
+++ src/resources.res
@@ -0,0 +1,11 @@
+BITMAP player_sprite "sprites/player.png" 0
+PALETTE player_pal "sprites/player.png" 0
+
+BITMAP enemy_sprite "sprites/enemy.png" 0
+PALETTE enemy_pal "sprites/enemy.png" 0
+
+BITMAP tree_sprite "sprites/tree.png" 0
+PALETTE tree_pal "sprites/tree.png" 0
+
+BITMAP sign_sprite "sprites/sign.png" 0
+PALETTE sign_pal "sprites/sign.png" 0
---
+++ src/track.c
@@ -0,0 +1,28 @@
+#include <genesis.h>
+#include "config.h"
+
+// Definição da pista
+const TrackSegment track_segments[] = {
+    { .curve = 0, .hill = 0, .length = 100 },
+    { .curve = 0, .hill = 0, .length = 100 },
+    { .curve = 0, .hill = 0, .length = 474 },
+    { .curve = 0, .hill = 0, .length = 100 },
+    { .curve = 0, .hill = 0, .length = 100 },
+    { .curve = 0, .hill = 0, .length = 100 },
+    { .curve = 0, .hill = 0, .length = 100 },
+    { .curve = 0, .hill = 0, .length = 100 },
+    { .curve = 0, .hill = 0, .length = 100 },
+    { .curve = 0, .hill = 0, .length = 100 },
+    { .curve = 0, .hill = 0, .length = 100 },
+    { .curve = 0, .hill = 0, .length = 100 },
+    { .curve = 0, .hill = 0, .length = 100 },
+    { .curve = 0, .hill = 0, .length = 100 },
+    { .curve = 0, .hill = 0, .length = 100 },
+    { .curve = 0, .hill = 0, .length = 100 },
+    { .curve = 0, .hill = 0, .length = 100 },
+    { .curve = 0, .hill = 0, .length = 100 },
+    { .curve = 0, .hill = 0, .length = 100 },
+    { .curve = 0, .hill = 0, .length = 100 }
+};
+
+const u16 num_segments = 20;
